3ec4bd2d668ba662f5d2b9c289410e07927bd405
patZii123 <66642274+patZii123@users.noreply.github.com>
2020-06-09 02:04:05 +0700

    Add files via upload

---
+++ test1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div><a href="SearchList.php">test 1.</a></div>
+        <div>test 2.
+            <ul>
+                <li><a href="loop1_1.php">1.1</a></li>
+                <li><a href="loop1_2.php">1.2</a></li>
+                <li><a href="test4.html">4.</a></li>
+            </ul>
+        </div>
+    </body>
+</html>
+
+
+
